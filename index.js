5cbdef4b73de4fbbeb74c037f41ec83240f9b3e1
Valentino Wong <valentino.wong@gmail.com>
2019-08-28 12:45:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+        name: userName,
+        email: userEmail
+      };
+       
+    let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(function(json) {
+            const userId = json.id;
+            const body = document.querySelector('body');
+            body.innerHTML = json.id
+        })
+        .catch(function(error) {
+            const body = document.querySelector('body');
+            body.innerHTML = error.message
+          });
+}
