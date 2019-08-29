7c7ce51e8dfb5c96e51ce1449d20ab11e2ab07d6
github username <github email address>
2019-08-29 07:23:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+document.addEventListener(ContentLoad)
+function submitData(name, email) {
+    let configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        document.body.innerHTML = object["id"];
+        
+      })
+      .catch(function(error) {
+        document.body.innerHTML = error.message;
+    });
+    
+}
+
+ 
+
+  
